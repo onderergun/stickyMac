b894799e1fe07e2c7b1125bd4980777cddb1c6e4
Ali Onder Ergun <43433494+onderergun@users.noreply.github.com>
2021-07-15 10:31:59 +0300

    Add files via upload

---
+++ sticky.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import time
+from jsonrpclib import Server
+
+def main():
+    api = Server("unix:/var/run/command-api.sock")    
+    logs = api.runCmds(1, ["show logging last 1 minutes"],"text" )
+    loglines = logs[0]["output"].split('\n')
+    interfaceUp= []
+    excludedinterfaces = [""]
+    for line in loglines:
+        if "LINEPROTO-5-UPDOWN" in line and "changed state to up" in line:
+            linesplit = line.split(" ")
+            for word in linesplit:
+                if ("Ethernet" in word) and (word not in excludedinterfaces):
+                    interfaceUp.append(word.strip(","))
+    if interfaceUp != []:
+        staticMac = api.runCmds(1, ["show mac address-table static"],"json" )[0]["unicastTable"]["tableEntries"]
+        staticMacinterface = []
+        for mac in staticMac:
+            staticMacinterface.append(mac["interface"])
+        for interface in interfaceUp:
+            if interface not in staticMacinterface:
+                macTableinterface = api.runCmds(1, ["show mac address-table interface " + interface],"json" )[0]["unicastTable"]["tableEntries"][0]
+                vlan = macTableinterface["vlanId"]
+                mac = macTableinterface["macAddress"]
+                api.runCmds(1, ["configure", "mac address-table static " + mac + " vlan " + str(vlan) + " interface " + interface])
+if __name__ == "__main__":
+    main()
+
+"""
+event-handler sticky
+   trigger on-intf Ethernet1-48 operstatus
+   delay 5
+   action bash python /mnt/flash/sticky.py
+
+management api http-commands
+   protocol unix-socket
+   no shutdown
+   !
+   vrf MGMT
+      no shutdown
+"""
