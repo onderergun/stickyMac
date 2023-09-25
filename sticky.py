c1ec0a45464b3d317168c4918db228d7bc2546f5
Ali Onder Ergun <43433494+onderergun@users.noreply.github.com>
2023-09-25 15:17:41 +0300

    Update sticky.py

--- sticky.py
+++ sticky.py
@@ -1,32 +1,40 @@
 #!/usr/bin/env python
-import time
 from jsonrpclib import Server
 
 def main():
-    api = Server("unix:/var/run/command-api.sock")    
-    logs = api.runCmds(1, ["show logging last 1 minutes"],"text" )
+    api = Server("unix:/var/run/command-api.sock")
+    logs = api.runCmds(1, ["show logging last 1 minutes"], "text")
     loglines = logs[0]["output"].split('\n')
-    interfaceUp= []
+    interfaceUp = []
     excludedinterfaces = [""]
+    portsec = api.runCmds(1, ["show port-security"],
+                          "json")[0]["portStatistics"]
     for line in loglines:
         if "LINEPROTO-5-UPDOWN" in line and "changed state to up" in line:
             linesplit = line.split(" ")
             for word in linesplit:
-                if ("Ethernet" in word) and (word not in excludedinterfaces):
+                if ("Ethernet" or "Port-Channel" in word) and (word.strip(",") not in excludedinterfaces) and (word.strip(",") in portsec):
                     interfaceUp.append(word.strip(","))
+
     if interfaceUp != []:
-        staticMac = api.runCmds(1, ["show mac address-table static"],"json" )[0]["unicastTable"]["tableEntries"]
+        staticMac = api.runCmds(
+            1, ["show mac address-table static"], "json")[0]["unicastTable"]["tableEntries"]
         staticMacinterface = []
         for mac in staticMac:
             staticMacinterface.append(mac["interface"])
         for interface in interfaceUp:
-            if interface not in staticMacinterface:
-                showMacinterface = api.runCmds(1, ["show mac address-table interface " + interface],"json" )[0]["unicastTable"]["tableEntries"]
-                if showMacinterface != []:
-                    macTableinterface = showMacinterface[0]
-                    vlan = macTableinterface["vlanId"]
-                    mac = macTableinterface["macAddress"]
-                    api.runCmds(1, ["configure", "mac address-table static " + mac + " vlan " + str(vlan) + " interface " + interface])
+            if staticMacinterface.count(interface) < portsec[interface]["maxSecureAddr"]:
+                dynamicMacinterface = api.runCmds(
+                    1, ["show mac address-table dynamic interface " + interface], "json")[0]["unicastTable"]["tableEntries"]
+                if dynamicMacinterface != []:
+                    for mac in dynamicMacinterface:
+                        vlan = mac["vlanId"]
+                        macaddr = mac["macAddress"]
+                        api.runCmds(1, ["configure", "mac address-table static " +
+                                        macaddr + " vlan " + str(vlan) + " interface " + interface])
+
+                    api.runCmds(1, ["write"])
+
 if __name__ == "__main__":
     main()
 
